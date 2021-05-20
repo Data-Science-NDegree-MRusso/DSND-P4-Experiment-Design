8ae2676efd6d336569ea54947efa16ba1670fede
Michelangelo Russo <michelangelo.russo@hotmail.com>
2021-05-20 11:18:59 +0200

    feat: started exploring data

--- test_results.py
+++ test_results.py
@@ -9,19 +9,19 @@ def score(df, promo_pred_col = 'Promotion'):
     irr = n_treat_purch / n_treat - n_ctrl_purch / n_control
     nir = 10 * n_treat_purch - 0.15 * n_treat - 10 * n_ctrl_purch
     return (irr, nir)
-    
+
 
 def test_results(promotion_strategy):
     test_data = pd.read_csv('Test.csv')
     df = test_data[['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7']]
     promos = promotion_strategy(df)
-    score_df = test_data.iloc[np.where(promos == 'Yes')]    
+    score_df = test_data.iloc[np.where(promos == 'Yes')]
     irr, nir = score(score_df)
     print("Nice job!  See how well your strategy worked on our test data below!")
     print()
     print('Your irr with this strategy is {:0.4f}.'.format(irr))
     print()
     print('Your nir with this strategy is {:0.2f}.'.format(nir))
-    
+
     print("We came up with a model with an irr of {} and an nir of {} on the test set.\n\n How did you do?".format(0.0188, 189.45))
     return irr, nir
